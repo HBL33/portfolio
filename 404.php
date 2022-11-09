3791531f63f23856d556b4347367911143120e85
Hervé Bonnaud <102042335+HBL33@users.noreply.github.com>
2022-11-09 09:34:03 +0100

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php
+require_once './php/includes/head.php';
+?>
+<!-- 404 -->
+<section id="work" class="tm-padding-top-bottom-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-1 col-md-11">
+                <h2 class="title">Not <strong>Found</strong> Erreur 404</h2>
+            </div>
+            <div class="col-md-12 col-sm-12">
+                <div class="work-wrapper">
+                    <i class="fa fa-link fa-4x"></i>
+                    <h3 class="text-uppercase tm-work-h3">Perdu!</h3>
+                    <hr>
+                    <a href="index.php">
+                        <p>Ne vous inquiètez pas, vous êtes toujours sur mon portfolio, cliquez sur ce lien pour revenir sur la bonne page</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- 404 -->
+
+</html>
---
+++ index.php
@@ -0,0 +1,370 @@
+<?php
+require_once './php/includes/head.php';
+require_once './php/includes/header.php';
+?>
+<!-- START work -->
+<section id="work" class="tm-padding-top-bottom-100">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-1 col-md-11">
+				<h2 class="title">Mon <strong>Travail</strong></h2>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div class="work-wrapper">
+					<i class="fa fa-link fa-4x"></i>
+					<h3 class="text-uppercase tm-work-h3">Responsive</h3>
+					<hr>
+					<p>Le design responsive est une approche de la conception web qui permet d’adapter votre contenu web aux
+						différentes tailles d’écran et de fenêtre d’une variété d’appareils.</p>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div class="work-wrapper">
+					<i class="fa fa-mobile"></i>
+					<h3 class="text-uppercase tm-work-h3">Web Mobile</h3>
+					<hr>
+					<p>Le Web Mobile est une technique qui permet d'accéder à Internet depuis un téléphone mobile, on parle
+						alors de l'Internet Mobile et des smartphones.</p>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<div class="work-wrapper">
+					<i class="fa fa-wordpress"></i>
+					<h3 class="text-uppercase tm-work-h3">Wordpress</h3>
+					<hr>
+					<p>WordPress est un logiciel libre utilisé pour créer de superbes sites, blogs ou applications. 43% du web
+						est propulsé par WordPress, des blogs aux plus gros sites d’actualités.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END work -->
+<!-- START PORTFOLIO -->
+<section id="portfolio" class="tm-portfolio">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 wow bounce">
+				<div class="title">
+					<h2 class="tm-portfolio-title">Mon <strong>Portfolio</strong></h2>
+				</div>
+				<!-- START ISO SECTION -->
+				<div class="iso-section">
+					<ul class="filter-wrapper clearfix">
+						<li><a href="#" class="opc-main-bg selected" data-filter="*">Responsive</a></li>
+						<li><a href="#" class="opc-main-bg" data-filter=".html">HTML</a></li>
+						<li><a href="#" class="opc-main-bg" data-filter=".photoshop">PHP</a></li>
+						<li><a href="#" class="opc-main-bg" data-filter=".wordpress">Wordpress</a></li>
+						<li><a href="#" class="opc-main-bg" data-filter=".mobile">Mobile</a></li>
+					</ul>
+					<div class="iso-box-section">
+						<div class="iso-box-wrapper col4-iso-box">
+							<div class="iso-box wordpress mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/effiloche-couture.png" class="fluid-img" alt="Atelier de couture à Ambazac">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">HTML</h3>
+										<p>Site réalisé avec Wordpress Elementor.<br><a class="linkwp" href="https://effiloche-couture.fr/" target="_blank">Création d'un site wordpress sous Elementor pour un Atelier de retouches et créations</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box wordpress mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/maisosgigandnew.png" class="fluid-img" alt="Maisons bois AMG Gigand">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">Wordpress</h3>
+										<p>Site réalisé avec wordpress Elementor.<br><a class="linkwp" href="https://constructions.maisonsgigand.fr/" target="_blank">Maisons Bois AMG Gigand</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box wordpress mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/la-table-de-3.png" class="fluid-img" alt="Site fictif restaurant">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">Elementor</h3>
+										<p>Site réalisé avec Wordpress Elementor<br><a class="linkwp" href="https://latablede3.bonnaudherve.fr/" target="_blank">Site fictif d'un restaurant aussi fictif! La Table de 3.</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box html mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/gothic-repro.png" class="fluid-img" alt="portfolio img">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">Html</h3>
+										<p>Reproduction graphique d'un site en HTML et Bootstrap.<br><a class="linkwp" href="/gothic/index1.html" target="_blank">Reproduction graphique du site "Gothic"</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box mobile wordpress col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/Emusic.bonnaudherve.fr.png" class="fluid-img" alt="reproduction site black silver">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">E-Commerce</h3>
+										<p>Realisation avec Wordpress et Woolentor.<br><a class="linkwp" href="https://emusic.bonnaudherve.fr/" target="_blank">Site fictif E-commerce d'une boutique d'instruments de musique</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box wordpress mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/espacemicro.png" class="fluid-img" alt="magasin d'informatique à Revel">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">Wordpress</h3>
+										<p>Refonte d'un site avec Wordpress.<br><a class="linkwp" href="https://espacemicro.net/" target="_blank">Espace Micro Revel, magasin d'informatique.</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box wordpress mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/arkitek.png" class="fluid-img" alt="portfolio img">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">Wordpress</h3>
+										<p>Réalisation d'un site avec Wordpress Elementor.<br><a class="linkwp" href="https://arkitek.bonnaudherve.fr/" target="_blank">ARKITEK "évènement architectural fictif"</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box html mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/wattwiller.png" class="fluid-img" alt="Maquetage sur Figma">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">HTML</h3>
+										<p>Reproduction du site Wattwiller en HTML pour un projet d'étude.<br><a class="linkwp" href="https://dev.bonnaudherve.fr/wattwiller/" target="_blank">Reproduction du site Wattwiller.</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box wordpress mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/bike-pro.png" class="fluid-img" alt="portfolio img">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">E-Commerce</h3>
+										<p>Réalisation d'un site avec Wordpress Elementor Woocommerce.<br><a class="linkwp" href="https://bike.site-forever.fr/" target="_blank">Bike-pro site E-commerce fictif boutique de vélos"</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box photoshop mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/mon-armoire.png" class="fluid-img" alt="portfolio img">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">PHP</h3>
+										<p>Réalisation d'un site en PHP type MVC pour une application web mobile<br><a class="linkwp" href="https://mon-armoire.bonnaudherve.fr/" target="_blank">Application web mobile Mon Armoire"</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box html mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/loustaldenparis.png" class="fluid-img" alt="portfolio img">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">HTML</h3>
+										<p>Réalisation d'un site en HTML et CSS.<br><a class="linkwp" href="https://loustaldenparis.com/" target="_blank">L'Oustal d'en Paris, chambres d'hôtes en Lauragais"</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="iso-box wordpress mobile col-md-3 col-sm-3 col-xs-12">
+								<div class="portfolio-thumb">
+									<img src="images/eurl-gigand.png" class="fluid-img" alt="portfolio img">
+									<div class="portfolio-overlay">
+										<h3 class="portfolio-item-title">WordPress</h3>
+										<p>Réalisation d'un site vitrine.(en cours de réalisation sous Elementor Wordpress)<br><a class="linkwp" href="https://eurl.maisonsgigand.com/" target="_blank">EURL Adrien GIGAND, entreprise de gros-oeuvres."</a></p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END PORTFOLIO -->
+<!-- START RESUME -->
+<section id="resume" class="tm-padding-top-bottom-100">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<h2 class="title">Mon <strong>Profil</strong></h2>
+				<p><span class="tm-info-label">Nom</span> Hervé Bonnaud</p>
+				<p><span class="tm-info-label">Addresse</span> Bègles, Nouvelle-Aquitaine, France</p>
+				<p><span class="tm-info-label">Tél</span> 06 49 26 95 13</p>
+				<p><span class="tm-info-label">Email</span> dev@bonnaudherve.fr</p>
+				<p><span class="tm-info-label">Website</span> <a href="https://dev.bonnaudherve.fr/" class="tm-red-text">dev.bonnaudherve.fr</a></p>
+			</div>
+			<div class="col-md-5 col-sm-5">
+				<h2 class="tm-portfolio-title">Mes <br><strong>Compétences</strong></h2>
+				<p>Aujourd'hui, j'ai démmaré une nouvelle aventure dans le domaine du Web. Ma formation Développeur Web & Web Mobile m'apporte de nouvelles connaissances dans de nombreux domaines, HTML5, CSS3, SCSS, Bootstrap5, PHP 8, Mysql, Javascript, webpack, WordPress, Elémentor, Woo Commerce, paiement en ligne, application mobile, E-mailing que je compte bien mettre au service des entreprises ou des particuliers.</p>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+	</div>
+</section>
+<!-- END RESUME -->
+<!-- START ABOUT -->
+<section id="about" class="tm-about">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-5">
+				<div class="row d-flex justify-content-center">
+					<div class="col-md-8 col-sm-8">
+						<h5 class="text-center m-5 ">Votre météo avec <a href="https://openweathermap.org/">openweathermap</a></h5>
+						<form>
+							<div class="form-group col-md-6">
+								<input type="text" placeholder="Votre ville" class="form-control" id="inputCity">
+							</div>
+							<div class="col-md-6">
+								<button type="submit" class="btn  form-control my-3">
+									Rechercher
+								</button>
+							</div>
+						</form>
+					</div>
+					<div class="col-md-12 col-sm-6 d-flex justify-content-center my-md-3 ">
+						<div class="box-container ">
+							<div id="city" class=" box rounded-circle m-md-1"></div>
+							<div id="temp" class=" box rounded-3 m-md-1"></div>
+							<div id="humidity" class=" box rounded-3 m-md-1"></div>
+							<div id="wind" class="box rounded-3 m-md-1"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-7">
+				<div class="title">
+					<h2>Hervé <strong>Bonnaud</strong></h2>
+					<h1 class="tm-red-text">Développeur <strong>Web</strong></h1>
+				</div>
+				<p> Mon parcours atypique, depuis le début de ma carrière, m'a beaucoup enrichi sur le plan social et professionnel.
+					Mon attirance pour le domaine digital depuis plus de vingt ans, m'oriente dans le développement web avec l'envie de me dépasser dans ce domaine, en tant que développeur web mobile.
+					A ce jour, j'ai déjà réalisé pour mon compte personnel plusieurs sites en WordPress, et aussi en HTML, PHP, que vous pouvez découvrir sur ce site.
+					Je peux réaliser des sites vitrines, des sites E-Commerce, des blogs en respectant votre charte graphique.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END ABOUT -->
+<!-- START SOCIAL -->
+<section id="social" class="tm-social">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-sm-4 wow rotateInUpLeft" data-wow-delay="0.3s">
+				<div class="media linkedin">
+					<a href="https://www.linkedin.com/in/hervebonnaud33/" target="_blank">
+						<div class="media-object pull-left">
+							<i class="fa fa-linkedin"></i>
+						</div>
+						<div class="media-body">
+							<h4 class="media-heading tm-social-title">Retrouvez-moi sur</h4>
+							<h3>Linkedin</h3>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4 wow rotateInUpLeft" data-wow-delay="0.6s">
+				<div class="media github">
+					<a href="https://github.com/HBL33" target="_blank">
+						<div class="media-object pull-left">
+							<i class="fa fa-brands fa-square-github fa-4x"></i>
+						</div>
+						<div class="media-body ps-5">
+							<h4 class="media-heading tm-social-title">Retrouvez-moi sur</h4>
+							<h3>Github</h3>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-4 wow rotateInUpLeft" data-wow-delay="0.9s">
+				<div class="media figma">
+					<a href="https://www.figma.com/proto/4WftU3Uhmo8bmOuibhmuoH/webdesign?node-id=184%3A2&scaling=scale-down&page-id=0%3A1" target="_blank">
+						<div class="media-object pull-left">
+							<i class="fa fa-brands fa-figma fa-4x"></i>
+						</div>
+						<div class="media-body">
+							<h4 class="media-heading tm-social-title">Créations</h4>
+							<h3>Figma</h3>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END SOCIAL -->
+<!-- START CONTACT -->
+<?php
+/* contact */
+//mon adresse mail
+$VotreAdresseMail = "dev@bonnaudherve.fr";
+// si le bouton "Envoyer" est cliqué
+if (isset($_POST['envoyer'])) {
+	//on vérifie que le champ mail est correctement rempli
+	if (empty($_POST['mail'])) {
+		echo "Le champ mail est vide";
+	} else {
+		//on vérifie que l'adresse est correcte
+		if (!preg_match("#^[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?@[a-z0-9_-]+((\.[a-z0-9_-]+){1,})?\.[a-z]{2,}$#i", $_POST['mail'])) {
+			echo "L'adresse mail entrée est incorrecte";
+		} else {
+			//on vérifie que le champ name est correctement rempli
+			if (empty($_POST['name'])) {
+				echo "Le champ nom est vide";
+			} else {
+				//on vérifie que le champ message n'est pas vide
+				if (empty($_POST['message'])) {
+					echo "Le champ message est vide";
+				} else {
+					//tout est correctement renseigné, on envoi le mail
+					//on renseigne les entêtes de la fonction mail de PHP
+					$Entetes = "MIME-Version: 1.0\r\n";
+					$Entetes .= "Content-type: text/html; charset=UTF-8\r\n";
+					$Entetes .= "From: dev.bonnaudherve.fr <" . $_POST['mail'] . ">\r\n"; //de préférence une adresse avec le même domaine de là où, vous utilisez ce code, cela permet un envoie quasi certain jusqu'au destinataire
+					$Entetes .= "Reply-To: dev.bonnaudherve.fr <" . $_POST['mail'] . ">\r\n";
+					//on prépare les champs:
+					$name = $_POST['name'];
+					$Mail = $_POST['mail'];
+					$Sujet = '=?UTF-8?B?' . base64_encode($_POST['name']) . '?='; //Cet encodage (base64_encode) est fait pour permettre aux informations binaires d'être manipulées par les systèmes qui ne gèrent pas correctement les 8 bits (=?UTF-8?B? est une norme afin de transmettre correctement les caractères de la chaine)
+					$Message = htmlentities($_POST['message'], ENT_QUOTES, "UTF-8"); //htmlentities() converti tous les accents en entités HTML, ENT_QUOTES Convertit en + les guillemets doubles et les guillemets simples, en entités HTML
+					//en fin, on envoi le mail
+					if (mail($VotreAdresseMail, $name, nl2br($Message), $Entetes)) { //la fonction nl2br permet de conserver les sauts de ligne et la fonction base64_encode de conserver les accents dans le titre
+						echo "Le mail à été envoyé avec succès!";
+					} else {
+						echo "Une erreur est survenue, le mail n'a pas été envoyé";
+					}
+				}
+			}
+		}
+	}
+}
+?>
+<section id="contact" class="tm-contact">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="title">Envoyez-moi <br><strong>un message</strong></h2>
+				<div class="fontaw">
+					<i class="fa-solid fa-envelope-open-text fa-3x"></i>
+				</div>
+				<hr>
+			</div>
+			<div class="col-md-1 col-sm-1">
+			</div>
+			<div class="col-md-10 col-sm-10">
+				<form action="#contact" method="post">
+					<div class="col-md-6 col-sm-6">
+						<input class="form-control" type="text" name="name" placeholder="Votre Nom">
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<input class="form-control" type="text" name="mail" value="" placeholder="Votre Email">
+					</div>
+					<div class="col-md-12 col-sm-6">
+						<textarea class="form-control" name="message" placeholder="Votre Message" rows="6"></textarea>
+					</div>
+					<div class="col-md-offset-2 col-md-8 col-sm-offset-2 col-sm-8">
+						<input class="form-control" type="submit" name="envoyer" value="ENVOYEZ LE MESSAGE">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<?php
+	require_once './php/includes/footer.php';
+
+	?>
+
+	</html>
